c5c53d5a091dc229739160f5b6eb50918d653d3b
Adam EM <adam.diavolo@gmail.com>
2022-12-13 12:24:24 +0200

    Initial Commit

---
+++ DubDubGrub/DubDubGrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubDubGrubApp.swift
+//  DubDubGrub
+//
+//  Created by Adam Essam on 13/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct DubDubGrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
