4ef38c8d38eebf62e4c76380c462a7197c94e68b
Adam EM <adam.diavolo@gmail.com>
2022-12-13 14:51:46 +0200

    setup MapView Screen

--- DubDubGrub/LocationMapView.swift
+++ DubDubGrub/LocationMapView.swift
@@ -6,10 +6,32 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct LocationMapView: View {
+    
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37.331516, longitude: -121.891054),
+                                                   span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+    
     var body: some View {
-        Text("Map View")
+        ZStack {
+            Map(coordinateRegion: $region)
+                .ignoresSafeArea()
+            
+           VStack{
+               Image("ddg-map-logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 70)
+                    .shadow(radius: 10)
+               Spacer()
+            }
+        }
+        .safeAreaInset(edge: .bottom, alignment: .center, spacing: 0) { /// to prevent the map from overlaping the tabview
+            Color.clear
+                .frame(height: 3)
+                .background(Material.bar)
+        }
     }
 }
 
